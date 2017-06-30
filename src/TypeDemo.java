704fe612a451aa4960a7e1cb10ae270bb97966b5
bhawani0642 <bhawani0642@gmail.com>
2017-06-30 15:52:49 +0530

    Assignment7.2

---
+++ src/TypeDemo.java
@@ -0,0 +1,17 @@
+// Created a class TypeDemo
+public class TypeDemo {
+	    // The main method of class
+		public static void main(String[] args) {
+			
+			//Taking two different type of variable short and double
+		    short s = 2000;
+		    double d = .1011;
+		    //promoting short into double
+		    double result = d * s;
+		    // printline to printing the result
+		    System.out.println("d * s is " + result);
+
+		}
+
+	}
+
